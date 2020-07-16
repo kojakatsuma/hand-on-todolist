087c430d9ebf8371ca2edf1cceaca217cf865988
kojakatsuma <katsuma.koja@ubiregi.com>
2020-07-16 10:03:00 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const App = () => {
+  const numberList = [1, 2, 3]
+  return numberList.map((num, i) => <Number key={i} num={num} />)
+}
+
+const Number = (props) => {
+  // props.num += // NG
+  const changedNum = props.num + 1
+  return <div>{changedNum}</div>
+}
