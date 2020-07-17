84d9d3fc21fb841d1ed198e992b83dc2e9f06452
kojakatsuma <katsuma.koja@ubiregi.com>
2020-07-17 10:28:55 +0900

    ここからハンズオンスタート

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,21 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export const TodoForm = (props) => {
-  const { createTodo } = props
-  const [name, setName] = useState('')
-  const [dueDate, setDueDate] = useState('')
-  const changeName = (e) => setName(e.target.value)
-  const changeDueDate = (e) => setDueDate(e.target.value)
-  return (
-    <>
-      <h2>TODO入力フォーム</h2>
-      <div>
-        <input value={name} onChange={changeName} />
-      </div>
-      <div>
-        <input value={dueDate} type="datetime-local" onChange={changeDueDate} />
-      </div>
-      <button onClick={() => createTodo(name, dueDate)}>作成</button>
-    </>
-  )
+  return <div>ここに作成フォームをつくる</div>
 }
