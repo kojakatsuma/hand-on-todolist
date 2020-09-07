d3b603001b6a4a4fc05db285e1f2b06210a151b1
kojakatsuma <katsuma.koja@ubiregi.com>
2020-09-08 01:05:07 +0900

    blocked

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -11,6 +11,14 @@ export const TodoList = () => {
   const [todos, setTodos] = useState(todosMock)
 
   const createTodo = (name, dueDate) => {
+    const startTime = Date.now()
+
+    while (true) {
+      const diffTime = Date.now() - startTime
+      if (diffTime >= 5000) {
+        break // 指定時間経過したら関数の実行を終了
+      }
+    }
     setTodos(todos.concat({ name, dueDate: new Date(dueDate) }))
   }
 
